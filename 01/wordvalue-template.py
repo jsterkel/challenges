6169e144af29cf13feaa8f966858ca8653c3b971
Justin Sterkel <js@3side.com>
2017-03-17 17:12:00 -0500

    Words are loaded from file into a list.

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -4,7 +4,13 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    list_of_words = []
+    dict_file = open(DICTIONARY,"r")
+    for line in dict_file: 
+        next_line = dict_file.readline()
+        list_of_words.append(next_line.strip())
+    return(list_of_words)
+
 
 def calc_word_value():
     """Calculate the value of the word entered into function
@@ -18,3 +24,8 @@ def max_word_value():
 
 if __name__ == "__main__":
     pass # run unittests to validate
+
+
+all_the_words = load_words()
+
+print(all_the_words)
